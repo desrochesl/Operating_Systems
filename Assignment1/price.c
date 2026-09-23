f15dda0db04780218b3afb988ee42899ef1e6b2f
Luna <luna@desroches.org>
2026-09-23 13:52:14 -0400

    Complete price.c

--- Assignment1/price.c
+++ Assignment1/price.c
@@ -1,7 +1,18 @@
-
+#include <stdio.h>
 /**
- * TODO: Complete and document
+ * Calculates the total price of an order.
+ * @param peritem price of each item
+ * @param count numbers of items bought
+ * @param bulk_limit minimum amount of items before discount
  */
 int price(int peritem, int count, int bulk_limit) {
-    return 0;
+    if (count < 0 || peritem < 0) {
+        return 0;
+    }
+    if (count < bulk_limit) {
+        return count * peritem;
+    }
+
+    int halfPrice = (peritem / 2) * (bulk_limit % count);
+    return (peritem / 2 * (count - bulk_limit) + (peritem * bulk_limit));
 }
