e061ab1581965c7fab261fb067a9aa56ebbf4c09
Luna <luna@desroches.org>
2026-09-23 13:52:38 -0400

    Complete power.c

--- Assignment1/power.c
+++ Assignment1/power.c
@@ -1,7 +1,16 @@
-
 /**
- * TODO: Complete and document
+ * Calculates x to the power of y
+ * @param x constant
+ * @param y exponent
  */
 int power(int x, int y) {
-    return 1;
+    if (y == 0) return 1;
+
+    if (x < 0 || y < 0) return 0;
+    int i;
+    int pow = x;
+    for (i = 1; i < y; i++) {
+        pow *= x;
+    }
+    return pow;
 }
