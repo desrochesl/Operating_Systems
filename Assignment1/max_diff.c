e0dd9d14c7cde4d3d99f5fe766d1a100b0db0705
Luna <luna@desroches.org>
2026-09-23 13:52:33 -0400

    Complete max_diff.c

--- Assignment1/max_diff.c
+++ Assignment1/max_diff.c
@@ -1,8 +1,20 @@
-#include <stdlib.h> // For NULL
+#include <stdlib.h>  // For NULL
 
 /**
- * TODO: Complete and document
+ * Calculates the difference between the smallest and largest integers in an
+ * array
+ * @param array list of numbers
+ * @param alen length of array
  */
 int max_diff(int* array, int alen) {
-    return 1;
+    if (alen < 1) return 0;
+    int min = 1000000000;
+    int max = -1000000000;
+    int i;
+
+    for (i = 0; i < alen; i++) {
+        if (array[i] < min) min = array[i];
+        if (array[i] > max) max = array[i];
+    }
+    return max - min;
 }
